67cdcb6f1e76bfde6afc2148162cd676e1c66531
Oualidlaib <laiboualid2003@gmail.com>
2026-04-28 07:26:56 +0100

    add Invoice data class

---
+++ app/src/main/java/app/Invoice.java
@@ -0,0 +1,16 @@
+package app;
+
+public class Invoice {
+    public final double total;
+    public final double discountAmount;
+    public final double taxAmount;
+    public final double finalPrice;
+
+    public Invoice(double total, double discountAmount,
+                   double taxAmount, double finalPrice) {
+        this.grossTotal     = total;
+        this.discountAmount = discountAmount;
+        this.taxAmount      = taxAmount;
+        this.finalPrice     = finalPrice;
+    }
+}
